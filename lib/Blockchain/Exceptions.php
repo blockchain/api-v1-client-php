b23a035f4ee6b1fedf8f1ff516461ded7989ecef
Shayan Es <shayanbahal@gmail.com>
2015-01-21 18:53:18 -0500

    FIX Uncaught exception
    PHP Fatal error:  Uncaught exception 'Blockchain_Error' with message
    'Unable to decode JSON response from Blockchain: <!DOCTYPE html>
    <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en-US">
    <![endif]-->
    <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en-US">
    <![endif]-->
    <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en-US">
    <![endif]-->
    <!--[if gt IE 8]><!--> <html class="no-js" lang="en-US">
    <!--<![endif]-->
    <head>
    
    <meta http-equiv="set-cookie" content="cf_use_ob=0; expires=Wed,
    21-Jan-15 23:09:12 GMT; path=/">
    
    <title>blockchain.info | 522: Connection timed out</title>
    <meta charset="UTF-8" />
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
    <meta name="robots" content="noindex, nofollow" />
    <meta name="viewport"
    content="width=device-width,initial-scale=1,maximum-scale=1" />
    <link rel="stylesheet" id="cf_styles-css"
    href="/cdn-cgi/styles/cf.errors.css" type="text/css"
    media="screen,projection" />
    <!--[if lt IE 9]><li in …/lib/Blockchain.php on line 116

--- lib/Blockchain/Exceptions.php
+++ lib/Blockchain/Exceptions.php
@@ -6,4 +6,14 @@ class Blockchain_ApiError extends Blockchain_Error {}
 class Blockchain_FormatError extends Blockchain_Error {}
 class Blockchain_ParameterError extends Blockchain_Error {}
 class Blockchain_CredentialsError extends Blockchain_Error {}
+
+function default_exception_handler(Exception $e){
+          // show something to the users letting them know something's down
+			// hacky fix on maintanence downtime on Jan 21,2015
+          echo ("{\"status\":\"error\",\"message\":\"Something went wrong! Please try later.\"}");
+ }
+ 
+ set_exception_handler("default_exception_handler");
+
+
 ?>
