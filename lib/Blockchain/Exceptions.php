f7be289a7226bec1d5455f492fbb048925bd020e
Shayanb <shayanbahal@gmail.com>
2015-04-17 11:43:35 -0400

    json_encode exception

--- lib/Blockchain/Exceptions.php
+++ lib/Blockchain/Exceptions.php
@@ -10,7 +10,12 @@ class Blockchain_CredentialsError extends Blockchain_Error {}
 function default_exception_handler(Exception $e){
           // show something to the users letting them know something's down
 			// hacky fix on maintanence downtime on Jan 21,2015
-          echo ("{\"status\":\"error\",\"reason\":\"".substr($e->getMessage(), 0, 50).".\"}");
+			$error = array(
+                "status" => "error",
+                "reason" => substr($e->getMessage(), 0, 100)
+                );
+
+          echo json_encode($book, JSON_PRETTY_PRINT);
  }
  
  set_exception_handler("default_exception_handler");
