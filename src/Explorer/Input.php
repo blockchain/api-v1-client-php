6f687cd4df0195eeb5c4e69c67f33d1495932ec4
Anton Minin <anton.a.minin@gmail.com>
2017-08-24 02:58:47 +0300

    Remove pointless comments

--- src/Explorer/Input.php
+++ src/Explorer/Input.php
@@ -1,35 +1,10 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-/**
- * Short File Description
- * 
- * PHP version 5
- * 
- * @category   aCategory
- * @package    aPackage
- * @subpackage aSubPackage
- * @author     anAuthor
- * @copyright  2014 a Copyright
- * @license    a License
- * @link       http://www.aLink.com
- */
+
 namespace Blockchain\Explorer;
 
-/**
- * Short Class Description
- * 
- * PHP version 5
- * 
- * @category   aCategory
- * @package    aPackage
- * @subpackage aSubPackage
- * @author     anAuthor
- * @copyright  2014 a Copyright
- * @license    a License
- * @link       http://www.aLink.com
- */
-class Input 
+class Input
 {
     /**
      * Properties
